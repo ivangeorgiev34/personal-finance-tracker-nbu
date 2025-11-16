7ff3226ea6bf6d75d8c18cfec2cfbe247359774a
Ivan Georgiev <ivan.georgiev0904@gmail.com>
2025-11-16 18:29:52 +0200

    Add transactions factories

---
+++ PersonalFinanceTracker.Core/Utils/ExpenseFactory.cs
@@ -0,0 +1,10 @@
+﻿using PersonalFinanceTracker.Core.Data;
+using PersonalFinanceTracker.Core.Data.Enums;
+
+namespace PersonalFinanceTracker.Core.Utils
+{
+    public class ExpenseFactory
+    {
+        public static Transaction Create(decimal amount, string description, DateTime date, ExpenseCategory category) => new Expense(amount, description, date, category);
+    }
+}
---
+++ PersonalFinanceTracker.Core/Utils/IncomeFactory.cs
@@ -0,0 +1,10 @@
+﻿using PersonalFinanceTracker.Core.Data;
+using PersonalFinanceTracker.Core.Data.Enums;
+
+namespace PersonalFinanceTracker.Core.Utils
+{
+    public class IncomeFactory
+    {
+        public static Transaction Create(decimal amount, string description, DateTime date, IncomeCategory category) => new Income(amount, description, date, category);
+    }
+}
