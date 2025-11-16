4cca9a44c9910184975131ed91109c92605db557
Ivan Georgiev <ivan.georgiev0904@gmail.com>
2025-11-16 18:35:26 +0200

    Add Account class for managing account for the application's runtime

---
+++ PersonalFinanceTracker.Core/Data/Account.cs
@@ -0,0 +1,32 @@
+﻿namespace PersonalFinanceTracker.Core.Data
+{
+    public class Account
+    {
+        private static Account _instance;
+        private static readonly object _lock = new object();
+        private Account()
+        {
+            Transactions = new List<Transaction>();
+        }
+
+        public static Account Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lock)
+                    {
+                        if (_instance == null) _instance = new Account();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        public decimal Balance { get; set; }
+
+        public ICollection<Transaction> Transactions { get; set; }
+    }
+}
