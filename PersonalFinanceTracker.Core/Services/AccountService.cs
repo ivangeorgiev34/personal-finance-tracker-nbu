ba82f4da6678ef39bbcde10a2abd605785a67225
Ivan Georgiev <ivan.georgiev0904@gmail.com>
2025-11-18 22:24:33 +0200

    Add account service

---
+++ PersonalFinanceTracker.Core/Services/AccountService.cs
@@ -0,0 +1,30 @@
+﻿using PersonalFinanceTracker.Core.Data;
+using PersonalFinanceTracker.Core.Data.Enums;
+using PersonalFinanceTracker.Core.Services.Contracts;
+using PersonalFinanceTracker.Core.Utils;
+
+namespace PersonalFinanceTracker.Core.Services
+{
+    public class AccountService : IAccountService
+    {
+        public Transaction AddExpense(decimal amount, string description, DateTime date, ExpenseCategory category)
+        {
+            var expense = ExpenseFactory.Create(amount, description, date, category);
+
+            Account.Instance.Transactions.Add(expense);
+            Account.Instance.Balance += amount;
+
+            return expense;
+        }
+
+        public Transaction AddIncome(decimal amount, string description, DateTime date, IncomeCategory category)
+        {
+            var income = IncomeFactory.Create(amount, description, date, category);
+
+            Account.Instance.Transactions.Add(income);
+            Account.Instance.Balance += amount;
+
+            return income;
+        }
+    }
+}
---
+++ PersonalFinanceTracker.Core/Services/Contracts/IAccountService.cs
@@ -0,0 +1,11 @@
+﻿using PersonalFinanceTracker.Core.Data;
+using PersonalFinanceTracker.Core.Data.Enums;
+
+namespace PersonalFinanceTracker.Core.Services.Contracts
+{
+    public interface IAccountService
+    {
+        Transaction AddIncome(decimal amount, string description, DateTime date, IncomeCategory category);
+        Transaction AddExpense(decimal amount, string description, DateTime date, ExpenseCategory category);
+    }
+}
