5e37cee59d333e1d4968f6e8fa237e90643d4b8d
Ivan Georgiev <ivan.georgiev0904@gmail.com>
2025-11-19 23:03:16 +0200

    Add Expense card component

---
+++ PersonalFinanceTracker/UI/Controls/ExpenseCard.cs
@@ -0,0 +1,57 @@
+﻿//Ivan Georgiev F114584
+namespace PersonalFinanceTracker.UI.Controls
+{
+    /// <summary>
+    /// Summary card control for displaying expense information
+    /// </summary>
+    public class ExpenseCard : SummaryCard
+    {
+        /// <summary>
+        /// Initializes the expense card with default styling
+        /// </summary>
+        public ExpenseCard()
+        {
+            Title = "Expenses";
+            IconBackColor = Color.FromArgb(254, 226, 226);
+            InitializeComponent();
+            SetupStyles();
+
+            this.Click += Card_Click;
+        }
+
+        /// <summary>
+        /// Updates the card data
+        /// </summary>
+        /// <param name="data"></param>
+        public override void UpdateData(params object[] data)
+        {
+            if (data.Length == 1)
+            {
+                decimal expenses = Convert.ToDecimal(data[0]);
+
+                Value = FormatCurrency(expenses);
+                lblValue.Text = Value;
+            }
+        }
+
+        /// <summary>
+        /// Sets the expense value displayed on the card
+        /// </summary>
+        /// <param name="expenses"></param>
+        public void SetExpenses(decimal expenses)
+        {
+            UpdateData(expenses);
+        }
+
+        /// <summary>
+        /// Handles card click to open expense transactions form
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected override void Card_Click(object sender, EventArgs e)
+        {
+            var form = new ExpenseTransactionsForm();
+            form.ShowDialog();
+        }
+    }
+}
