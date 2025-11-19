f8a12b7da239f8472c347fc521309186d1fcd17c
Ivan Georgiev <ivan.georgiev0904@gmail.com>
2025-11-19 23:06:02 +0200

    Add program entry point

---
+++ PersonalFinanceTracker/Program.cs
@@ -0,0 +1,19 @@
+//Ivan Georgiev F114584
+namespace PersonalFinanceTracker
+{
+    /// <summary>
+    /// Main program
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// App starting point
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new MainForm());
+        }
+    }
+}
